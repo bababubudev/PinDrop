abf1a2c119cf02dc153a0f314637521fc2baf497
Prabesh Sharma <prwshshrm@gmail.com>
2024-11-30 20:28:15 +0200

    Initial Commit

---
+++ PointToPin/PointToPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointToPinApp.swift
+//  PointToPin
+//
+//  Created by Prativa Sharma on 30.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct PointToPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
